8189e35cd9cf32be04e83042ce842aa351e05c0d
pratima <pratima.mohanty@targetintegration.com>
2016-03-14 11:48:50 +0000

    Added the file to create car1 database and car table inside car1 database, car table will store all the feature of the car

---
+++ sql/Create_Car1_Database.sql
@@ -0,0 +1,12 @@
+Create Database Car1;
+
+
+create table car1.car
+(
+carName varchar(20),
+carID int(10) primary key,
+companyName varchar(20),
+hasABS boolean,
+hasSpareTire boolean,
+hasWarranty boolean
+);
