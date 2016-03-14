ebfcfcddb796160ebd1ae3044d671c6bd4a717ae
pratima <pratima.mohanty@targetintegration.com>
2016-03-14 12:44:59 +0530

    insert record into table functionality of car2 database

---
+++ sql/insert_car2_functionality.sql
@@ -0,0 +1,11 @@
+
+
+
+insert into car2.functionality (feature,value,carID) values ('sideairbag',1,1);
+insert into car2.functionality (feature,value,carID) values ('autoGear',0,2);
+insert into car2.functionality (feature,value,carID) values ('sideairbag',1,3);
+insert into car2.functionality (feature,value,carID) values ('aircondition',0,4);
+insert into car2.functionality (feature,value,carID) values ('skidcontrol',1,5);
+insert into car2.functionality (feature,value,carID) values ('aircondition',0,2);
+insert into car2.functionality (feature,value,carID) values ('skidcontrol',1,3);
+select * from car2.functionality
