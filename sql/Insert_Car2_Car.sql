2fbc8335574318169bfd6acd064a12fd32509469
pratima <pratima.mohanty@targetintegration.com>
2016-03-15 04:03:08 +0000

    insert record in table car of car2 database

---
+++ sql/Insert_Car2_Car.sql
@@ -0,0 +1,8 @@
+insert into car2.car (carName,companyName) values ('BMW','automative');
+insert into car2.car (carName,companyName) values ('verna','honda');
+insert into car2.car (carName,companyName) values ('audi','automative');
+insert into car2.car (carName,companyName) values ('innova','toyota');
+insert into car2.car (carName,companyName) values ('dusto','mehandra');
+insert into car2.car (carName,companyName) values ('scorpio','mehandra');
+insert into car2.car (carName,companyName) values ('nano','tata');
+insert into car2.car (carName,companyName) values ('camry','toyota');
