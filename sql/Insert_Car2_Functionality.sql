42d04a7d5d945eaf508a2504acf0a4c9ad8046b6
pratima <pratima.mohanty@targetintegration.com>
2016-03-15 06:29:29 +0000

    insert record in functionality table of car2 database which contain forign keys

---
+++ sql/Insert_Car2_Functionality.sql
@@ -0,0 +1,8 @@
+insert into car2.functionality (feature_name,value,carID) values ('sideairbag',1,1);
+insert into car2.functionality (feature_name,value,carID) values ('autoGear',0,2);
+insert into car2.functionality (feature_name,value,carID) values ('sideairbag',1,3);
+insert into car2.functionality (feature_name,value,carID) values ('aircondition',0,4);
+insert into car2.functionality (feature_name,value,carID) values ('skidcontrol',1,5);
+insert into car2.functionality (feature_name,value,carID) values ('aircondition',0,2);
+insert into car2.functionality (feature_name,value,carID) values ('skidcontrol',1,3);
+insert into car2.functionality (feature_name,value,carID) values ('aircondition',1,3);
